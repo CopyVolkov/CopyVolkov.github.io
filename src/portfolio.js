62435f451ec06f5709423589e20a983d9e4162bf
Camilo <juan.chavez.galeano@correounivalle.edu.co>
2025-10-17 02:52:35 -0500

    Adding my portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,21 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  title: 'Juan Dev.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Juan Chavez',
+  role: 'Front End Developer',
+  picture: '/juan.jpg',
+
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'As a software engineering student my focus is on crafting captivating user experiences, driven by creativity. I specialize in creating simple yet impactful web pages with exceptional delivery, capable of conveying messages in a unique way.',
+  resume: '/DevCV.pdf', // if no resume, the button will not show up
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/akavolkov',
+    github: 'https://github.com/CopyVolkov',
   },
 }
 
@@ -21,28 +23,31 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Weather App',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A very simple weather app that shows the current weather and forecast for the city that´s inputed by the user using OpenWeather API',
+    stack: ['Vanilla Javascript', 'Tailwind CSS', 'HTML'],
+    sourceCode: 'https://github.com/CopyVolkov/CopyVolkov.github.io',
+    livePreview: 'https://copy-volkov-github-io-27qf.vercel.app',
+    image: 'proyect1.png',
   },
   {
-    name: 'Project 2',
+    name: 'To do list App',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A simple to-do list app that allows users to add, edit, and delete tasks simulating a local ran API with JSON Server',
+    stack: ['Tailwind CSS', 'Vanilla Javascript', 'React'],
+    sourceCode: 'https://www.linkedin.com/feed/update/urn:li:activity:7329675986277445632/?originTrackingId=G3PJI%2FSEQpWoFRCOn47rcg%3D%3D',
+    livePreview: '',
+    image: 'proyect2.png',
   },
   {
-    name: 'Project 3',
+    name: 'Rate it!',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A web game project that I\'m currently working on to improve my skills in Typescript, it-s a game based on the classic "Higher or Lower but this one uses game ratings instead of google searches.',
+    stack: ['Tailwind CSS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com/CopyVolkov/Rate.io',
+    livePreview: '',
+    image: 'project3.png',
   },
 ]
 
@@ -54,18 +59,15 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Astro',
+  'Tailwind CSS',
+  'POSTMAN',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'blackfire748@mail.com',
 }
 
 export { header, about, projects, skills, contact }
